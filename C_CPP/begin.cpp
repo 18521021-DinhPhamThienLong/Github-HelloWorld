a16f569fdfd4fcd46cbc8f5223e74659565edd68
Thien Long <18521021@gm.uit.edu.vn>
2021-03-09 21:31:02 +0700

    Create code

---
+++ C_CPP/begin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a = 10, b = 3;
+    int c;
+    c = a + b; 
+    cout << "Hello World " << c << endl;
+    cout << "Anh yeu em";
+    return 0;
+}
